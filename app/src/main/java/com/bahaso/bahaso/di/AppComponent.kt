787e0e777ae35aa7927091dcdf4c0d445f434663
raffifu <raffifebriandikau@gmail.com>
2021-06-06 10:04:28 +0700

    Create Functionality LogOut
    1. Add navigation ProfileFragment to LoginFragment
    2. Create onClickListener when button loguout pressed

--- app/src/main/java/com/bahaso/bahaso/di/AppComponent.kt
+++ app/src/main/java/com/bahaso/bahaso/di/AppComponent.kt
@@ -4,6 +4,7 @@ import com.bahaso.bahaso.core.di.CoreComponent
 import com.bahaso.bahaso.home.HomeFragment
 import com.bahaso.bahaso.quiz.QuizFragment
 import com.bahaso.bahaso.signup.SignUpFragment
+import com.bahaso.bahaso.profile.ProfileFragment
 import dagger.Component
 
 @AppScope
@@ -21,4 +22,5 @@ interface AppComponent {
     fun inject(fragment: HomeFragment)
     fun inject(fragment: QuizFragment)
     fun inject (fragment: SignUpFragment)
+    fun inject (fragment: ProfileFragment)
 }
--- app/src/main/java/com/bahaso/bahaso/profile/ProfileFragment.kt
+++ app/src/main/java/com/bahaso/bahaso/profile/ProfileFragment.kt
@@ -1,32 +1,42 @@
 package com.bahaso.bahaso.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bahaso.bahaso.BaseFragment
+import com.bahaso.bahaso.MyApplication
 import com.bahaso.bahaso.R
 import com.bahaso.bahaso.core.session.Preferences
+import com.bahaso.bahaso.core.ViewModelFactory
 import com.bahaso.bahaso.databinding.FragmentProfileBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import javax.inject.Inject
 
-class ProfileFragment : Fragment() {
-
+class ProfileFragment : BaseFragment() {
     private var _binding: FragmentProfileBinding? = null
 
     private val binding
-        get() = _binding!!
+        get() = _binding
+
+    @Inject
+    lateinit var factory: ViewModelFactory
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as MyApplication).appComponent.inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentProfileBinding.inflate(layoutInflater)
-        return _binding!!.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,8 +44,19 @@ class ProfileFragment : Fragment() {
 
         val pref = Preferences(requireContext())
 
-        binding.emailProfile.text = pref.email
-        binding.namaProfile.text = pref.name
-        binding.tanggalProfile.text = pref.birth
+        binding?.emailProfile?.text = pref.email
+        binding?.namaProfile?.text = pref.name
+        binding?.tanggalProfile?.text = pref.birth
+
+        binding?.btnLogout?.setOnClickListener {
+            pref.logout()
+            Firebase.auth.signOut()
+            findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToLoginFragment())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

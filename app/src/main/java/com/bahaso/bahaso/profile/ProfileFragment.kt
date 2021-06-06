b59b376dbc6b45997285ec4052d6831a9c92c6b3
Rzkykhrllh <rizkykirigaya@gmail.com>
2021-06-06 10:30:44 +0800

    Creating SharedPreference
    1. shared SharedPreference to save user indentity
    2. using sharedPreference to show data in profile fragment
    3. creating ProfileFragment.kt and ProfileViewModel.kt

--- app/src/main/java/com/bahaso/bahaso/profile/ProfileFragment.kt
+++ app/src/main/java/com/bahaso/bahaso/profile/ProfileFragment.kt
@@ -6,14 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bahaso.bahaso.R
+import com.bahaso.bahaso.core.session.Preferences
+import com.bahaso.bahaso.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(layoutInflater)
+        return _binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pref = Preferences(requireContext())
+
+        binding.emailProfile.text = pref.email
+        binding.namaProfile.text = pref.name
+        binding.tanggalProfile.text = pref.birth
     }
 }

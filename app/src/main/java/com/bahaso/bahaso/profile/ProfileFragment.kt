5f23d529f2a46f41ead8e569fb5f619341555af2
raffifu <raffifebriandikau@gmail.com>
2021-06-07 12:46:37 +0700

    Add Feature random profile

--- app/src/main/java/com/bahaso/bahaso/profile/ProfileFragment.kt
+++ app/src/main/java/com/bahaso/bahaso/profile/ProfileFragment.kt
@@ -1,18 +1,18 @@
 package com.bahaso.bahaso.profile
 
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import com.bahaso.bahaso.BaseFragment
 import com.bahaso.bahaso.MyApplication
-import com.bahaso.bahaso.R
 import com.bahaso.bahaso.core.ViewModelFactory
 import com.bahaso.bahaso.core.session.Preferences
 import com.bahaso.bahaso.databinding.FragmentProfileBinding
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
@@ -45,6 +45,13 @@ class ProfileFragment : BaseFragment() {
 
         val pref = Preferences(requireContext())
 
+        val user_id = Firebase.auth.currentUser?.uid.toString()
+
+        Glide
+            .with(this)
+            .load(getURLProfile(user_id))
+            .into(binding!!.fotoProfile)
+
         binding?.emailProfile?.text = pref.email
         binding?.namaProfile?.text = pref.name
         binding?.tanggalProfile?.text = pref.birth
@@ -71,5 +78,13 @@ class ProfileFragment : BaseFragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun getURLProfile(user_id: String) : String {
+        val builder: Uri.Builder = Uri.Builder()
+        builder.scheme("https")
+            .authority("robohash.org")
+            .appendPath(user_id + ".png")
+        return builder.build().toString()
     }
 }

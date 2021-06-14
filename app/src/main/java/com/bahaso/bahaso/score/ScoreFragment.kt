155158a1e7da139ac3c2c3d5e3654aca6a4090b8
alimahfud01 <alimahfud2001@gmail.com>
2021-06-14 22:24:09 +0700

    Score

--- app/src/main/java/com/bahaso/bahaso/score/ScoreFragment.kt
+++ app/src/main/java/com/bahaso/bahaso/score/ScoreFragment.kt
@@ -1,5 +1,6 @@
 package com.bahaso.bahaso.score
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,10 @@ import androidx.navigation.fragment.navArgs
 import com.bahaso.bahaso.BaseFragment
 import com.bahaso.bahaso.core.session.Preferences
 import com.bahaso.bahaso.databinding.FragmentScoreBinding
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class ScoreFragment : BaseFragment(){
     private var _binding: FragmentScoreBinding? = null
@@ -30,6 +35,7 @@ class ScoreFragment : BaseFragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val pref = Preferences(requireContext())
+        val user_id = Firebase.auth.currentUser?.uid.toString()
         binding?.scoreName?.text  = pref.name
         binding?.scoreGrade?.text = arguments.scoreArg.toString()
 
@@ -38,12 +44,24 @@ class ScoreFragment : BaseFragment(){
             findNavController().navigate(action)
         }
 
+        Glide
+            .with(this)
+            .load(getURLProfile(user_id))
+            .into(binding!!.scoreImageProfile)
+
         binding!!.btnTryAgain.setOnClickListener {
             val action = ScoreFragmentDirections.actionScoreFragmentToQuizFragment()
             action.topic = arguments.topic
             findNavController().navigate(action)
         }
-
-        }
     }
+
+    fun getURLProfile(user_id: String) : String {
+        val builder: Uri.Builder = Uri.Builder()
+        builder.scheme("https")
+            .authority("robohash.org")
+            .appendPath(user_id + ".png")
+        return builder.build().toString()
+    }
+}
 
